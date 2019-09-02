c663fee33bb7613057c9d21ade4cf1241436fcd3
Borshon96 <sajid.borshon@northsouth.edu>
2019-09-02 15:10:05 +0600

    new tab added

--- Project 2/Selenium_Testing/src/demo_testing/startApplication.java
+++ Project 2/Selenium_Testing/src/demo_testing/startApplication.java
@@ -1,5 +1,6 @@
 package demo_testing;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,6 +12,12 @@ public class startApplication {
 		
 		driver.get("https://phptravels.com/demo/");
 		driver.manage().window().maximize();
+		
+		driver.findElement(By.cssSelector("#Main > section.grey-box > div > div > div:nth-child(2) > div > div > div.col-md-9 > div.col-md-12 > div > div:nth-child(1) > div > a")).click();
+		
+		
+		//#Main > section.grey-box > div > div > div:nth-child(2) > div > div > div.col-md-9 > div.col-md-12 > div > div:nth-child(1) > div > a
+		
 
 	}
 
