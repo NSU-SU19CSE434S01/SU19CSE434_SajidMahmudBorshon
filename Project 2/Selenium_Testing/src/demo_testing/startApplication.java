2ff21a158cf78a1aee92b6a7ded65ab73fefb003
Borshon96 <sajid.borshon@northsouth.edu>
2019-09-02 14:49:44 +0600

    1st commit lib added and web driver added & open website

---
+++ Project 2/Selenium_Testing/src/demo_testing/startApplication.java
@@ -0,0 +1,17 @@
+package demo_testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class startApplication {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("https://phptravels.com/demo/");
+		
+
+	}
+
+}
