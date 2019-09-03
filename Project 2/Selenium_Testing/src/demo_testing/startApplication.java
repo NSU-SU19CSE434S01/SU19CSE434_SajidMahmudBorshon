71b8889c7f1ca7c9f86c8cd9fd056a1a9283749e
Borshon96 <sajid.borshon@northsouth.edu>
2019-09-03 23:03:15 +0600

    all the library and function name declared
    also opens the browser and open the website and maximize the window

--- Project 2/Selenium_Testing/src/demo_testing/startApplication.java
+++ Project 2/Selenium_Testing/src/demo_testing/startApplication.java
@@ -1,24 +1,58 @@
 package demo_testing;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
 
-public class startApplication {
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
+public class Application_launch {
+	
+	static WebDriver driver;
+	
+	static JavascriptExecutor jse;
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver.exe");
-		WebDriver driver= new ChromeDriver();
 		
-		driver.get("https://phptravels.com/demo/");
-		driver.manage().window().maximize();
-		
-		driver.findElement(By.cssSelector("#Main > section.grey-box > div > div > div:nth-child(2) > div > div > div.col-md-9 > div.col-md-12 > div > div:nth-child(1) > div > a")).click();
+		Application_launch test = new Application_launch();
+		test.invokeBrowser();
 		
 		
-		//#Main > section.grey-box > div > div > div:nth-child(2) > div > div > div.col-md-9 > div.col-md-12 > div > div:nth-child(1) > div > a
-		
-
 	}
+	
+	public void invokeBrowser() {
 
-}
+		
+		try {
+			System.setProperty("webdriver.chrome.driver", ".\\Drivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().deleteAllCookies();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+			
+			driver.get("https://www.phptravels.net");
+			
+			setInputField();
+			signupLogin();
+			logIn();
+			Thread.sleep(3000);
+			driver.close();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	
+	}
