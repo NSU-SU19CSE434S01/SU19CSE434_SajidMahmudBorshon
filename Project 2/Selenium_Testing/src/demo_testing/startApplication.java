6df9145587d31a2be2891621773a68d1cf21afd8
Borshon96 <sajid.borshon@northsouth.edu>
2019-09-04 08:47:25 +0600

    added extra text field filled for setinputfield function

--- Project 2/Selenium_Testing/src/demo_testing/startApplication.java
+++ Project 2/Selenium_Testing/src/demo_testing/startApplication.java
@@ -145,6 +145,11 @@ public class Application_launch {
 			jse = (JavascriptExecutor)driver;
 			jse.executeScript("scroll(0,200)");
 			Thread.sleep(1000);
+			
+			driver.findElement(By.xpath("//*[@id=\"guestform\"]/div[8]/div/div[1]")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"special\"]/div/textarea")).sendKeys("I wish to have authentic Bangladeshi food all the time");			
+			Thread.sleep(2000);
 		}
 	}
 }
