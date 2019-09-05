f9cd73bb15df6f2379eeaf460214c89308e5f8cd
Borshon96 <sajid.borshon@northsouth.edu>
2019-09-05 08:31:51 +0600

    contact no added in signupLogin function

--- Project 2/Selenium_Testing/src/demo_testing/startApplication.java
+++ Project 2/Selenium_Testing/src/demo_testing/startApplication.java
@@ -195,6 +195,8 @@ public class Application_launch {
 			driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[3]/input")).sendKeys("Sajid Mahmud");
 			Thread.sleep(1000);
 			driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[4]/input")).sendKeys("Borshon");
+			Thread.sleep(1000);
+			driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[5]/input")).sendKeys("01791912104");
 		}
 	}
 }
